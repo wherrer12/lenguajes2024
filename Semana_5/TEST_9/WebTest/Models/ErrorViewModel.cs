4bd499c85c15c2a6bcaaf58e0956dbb1ac8223aa
Willliam Herrera Rodríguez <william.herrera@ucr.ac.cr>
2024-04-05 21:26:15 -0600

    borrar test semana 5

--- Semana_5/TEST_9/WebTest/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebTest.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
