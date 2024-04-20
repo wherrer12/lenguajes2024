6bfefc1bc0328cbd5ed91768dad5cd3d83fa97e6
Willliam Herrera Rodríguez <william.herrera@ucr.ac.cr>
2024-04-19 20:27:14 -0600

    Clase 12, semana 6

---
+++ Semana_6/Clase_12_LOGIN/AppBibliotecaWebG1_v2/Controllers/LibrosController.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+//Se referencia el ORM
+using AppBibliotecaWebG1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppBibliotecaWebG1.Controllers
+{
+    public class LibrosController : Controller
+    {
+
+        //Variable que permite manejar la referencia del contexto
+        private readonly DbContextBiblioteca _context = null;
+
+        /// <summary>
+        /// Constructor con parametros que recibe la referencia del ORM
+        /// </summary>
+        /// <param name="context"></param>
+        /// 
+        public LibrosController(DbContextBiblioteca context)
+        {
+            _context = context;//Se asigna la referencia al contexto
+        }
+
+        public async Task<IActionResult> Index() //asincrinono por  ir en la bd
+        {
+
+            //Se declara la variable lista
+            //Por medio de ORM se lee la informacion de todos los libros en la BD
+            var listado = await _context.Libros.ToListAsync();
+
+            return View(listado);//Se envia el listado al front end
+        }
+
+        //Metodo encargado para almacenar libro
+        [HttpGet]
+        public IActionResult Create()//Metodo encargado de mostrar el front end para crear un libro
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Create([Bind] Libro libro)
+        {
+            if (libro == null)//Se valida que el objeto libro tenga datos
+            {
+                return View();//Como no hay datos, se deja al usuario dentro del formulario create
+            }
+            else { 
+
+                //Se asigna cero para generar el ISBN automatico
+                libro.ISBN = 0;
+
+                //Si hay datos alamcenados en el libro
+                _context.Libros.Add(libro);//Se agrega el libro al contexto
+
+                //Se aplican cambios en la BD
+                await _context.SaveChangesAsync();
+
+                //Ubicamos al usuario dentro del listado libros
+                return RedirectToAction("Index");
+            
+            }
+        }
+
+        //Metodo para el proceso de eliminacion de un libro
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            //Buscar el libro a eliminar por medio del ORM
+            var temp = await _context.Libros.FirstOrDefaultAsync(r => r.ISBN == id); 
+
+            //Se envian los datos al front end
+            return View(temp);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            //Se busca por ISBN el libro a eliminar
+            var temp = await _context.Libros.FirstOrDefaultAsync(r => r.ISBN == id);
+
+            //Se valida si tiene datos
+            if (temp != null)
+            {
+
+                //Se elimina el libro
+                _context.Libros.Remove(temp);
+
+                //Se aplican los cambios en la BD
+                await _context.SaveChangesAsync();
+
+                //Se redirige al usuario al listado de libros
+                return RedirectToAction("Index");
+                
+            }
+            else
+            {
+                return NotFound();//Error 404, recurso no disponible
+            }
+            
+        }
+
+        //Metodo encargado de mostrar los datos para un libro especifico
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            //Se busca el libro por ISBN
+            var temp = await _context.Libros.FirstOrDefaultAsync(r => r.ISBN == id);
+
+            //Se envian los datos al front end
+            return View(temp);
+        }
+
+        //Metodo encargado de realizar el proceso de editar de los datos de un libro
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            //Se busca el libro por ISBN
+            var temp = await _context.Libros.FirstOrDefaultAsync(r => r.ISBN == id);
+
+            //Se envian los datos del libro al front end
+            return View(temp);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind] Libro pLibro)
+        {
+            //Se valida el ID
+            if (id == pLibro.ISBN)
+            {
+                //Se busca el libro anterior con sus datos
+                var temp = await _context.Libros.FirstOrDefaultAsync(r => r.ISBN == id);
+
+                //Se elimina el libro
+                _context.Libros.Remove(temp);
+
+                //Se aplican los cambios en la BD
+                _context.Libros.Add(pLibro);
+
+                //Se aplican los cambios en la BD
+                await _context.SaveChangesAsync();
+
+                //Se redirige al usuario dentro del listado de libros
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+
+                return NotFound();//Error 404, recurso no disponible
+
+            }
+        }   
+
+    }//Fin de la clase
+}//Fin namespace
---
+++ Semana_6/Clase_12_LOGIN/AppBibliotecaWebG1_v2/Controllers/UsuariosController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AppBibliotecaWebG1.Models;
+
+namespace AppBibliotecaWebG1.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        //Variable para manejar la referencia del ORM
+        private readonly DbContextBiblioteca _context = null;
+
+        //Constructor con parametros
+
+        public UsuariosController(DbContextBiblioteca pContext)
+        {
+            _context = pContext; //Se asigna la referencia del ORM
+        }
+
+        /// <summary>
+        /// Metodo encargado de mostrar el formulario de autenticacion
+        /// </summary>
+        /// <returns></returns>
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+
+        /// <summary>
+        /// Metodo encargado de mostrar el front end para crear una cuenta a un usuario
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult CrearCuenta()
+        {
+            return View();
+        }
+
+        //Metodo encargado de crear una cuenta de usuario
+        public async Task<IActionResult> CrearCuenta([Bind]Usuario pUser) {
+
+            //Se valida si tiene datos
+            if (pUser == null)
+            {
+                return NotFound(); //Error 404, recurso no disponible
+            }
+            else { 
+            
+                //Se valida si tiene datos
+                pUser.FechaRegistro = DateTime.Now; //Se asigna la fecha registro
+                pUser.Estado = 'A'; //Se indica que su estado activo
+                pUser.Restablecer = 'S'; //Se indica que se debe restablecer la clave
+
+                //Se genera la clave temporal
+                pUser.Password = GenerarClave();
+
+                //Se agrega el usuario al catalogo
+                _context.Usuarios.Add(pUser);
+
+                try //Se intenta aplicar cambios y enviar el email
+                {
+                    //Se aplican los cambios
+                    await _context.SaveChangesAsync();
+
+                    //Se crea una instancia del objeto email
+                    Email email = new Email();
+
+                    //Se envia el como parametros los datos del usuario que recibe la clave temporal
+                    email.Enviar(pUser);
+
+                    //Si todo esta bien se logro enviar el correo
+                    //Se ubica al usuario dentro del formulario de login
+                    return RedirectToAction("Login", "Usuarios");
+
+                }
+                catch (Exception ex)//Ojo la variable ex almacena la informacion del error
+                {
+                    TempData["Mensaje"] = "Error al crear la cuenta..<br>" +
+                        "Verifique el siguiente mensaje de error: " + ex.Message;
+
+                    return View(); //Se ubica al usuario dentro del formulario de crear cuenta
+                }
+            
+            }
+
+        }
+
+        //Metodo encargado de generar un valor aleatorio
+        public string GenerarClave()
+        {
+            //Variable para generar un valor aleatorio
+            Random rnd = new Random();
+
+            //Variable para almacenar la clave
+            string clave = string.Empty;
+
+            //Caracteres que se pueden utilizar para generar la clave temporal
+            clave = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+            //Se genera la clave aleatoria
+            return new string(Enumerable.Repeat(clave, 12).Select(
+                s => s[rnd.Next(s.Length)]).ToArray());
+
+        }
+
+    }
+}
