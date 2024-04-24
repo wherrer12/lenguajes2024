9b01f0fc18d3cb23c4e2bb49754ae21627dc7697
wherrer12 <william.herrera@ucr.ac.cr>
2024-04-23 22:21:02 -0600

    Clase 13 - semana 7

---
+++ Semana_6/Clase_12_LOGIN/Sentencias clase 12.sql
@@ -0,0 +1,5 @@
+select * from Usuarios
+
+delete from Usuarios where email = 'wherrer12@gmail.com'
+
+delete from Usuarios where email = 'wherrer1@hotmail.com'
---
+++ Semana_7/Sesion_13_Autenticacion/AppBibliotecaWebG1_v2/Models/Email.cs
@@ -0,0 +1,85 @@
+﻿using System.Net.Mail;
+using System.Net.Mime;
+using System.Net;
+using System.Text;
+
+namespace AppBibliotecaWebG1.Models
+{
+    public class Email
+    {
+        //Metodo encargado de enviar la clave tempral al usuario
+        public void Enviar(Usuario usuario)
+        {
+            try
+            {
+                //Se crea la instancia del objeto Mail
+                MailMessage email = new MailMessage();
+
+                //Se indica el asunto del correo
+                email.Subject = "Datos de registro en plataforma web biblioteca CR";
+
+                //Se agrega al email el correo del destinatario
+                email.To.Add(new MailAddress(usuario.Email));
+
+                //Se copia el administrador de la cuenta
+                email.Bcc.Add(new MailAddress("Lenguajes2023G2@Outlook.com"));
+
+                //Se indica el emisor
+                email.From = new MailAddress("Lenguajes2023G2@Outlook.com");
+
+                //Se construye el html para el cuerpo del correo
+                string html = "Bienvenidos a biblioteca web CR, gracias por formar parte de nuestra plataforma";
+                html += "<br>A continuacion detallamos los datos registrados en nuestra plataforma web ";
+                html += "<br><b>Email: </b> " + usuario.Email;
+                html += "<br><b>Nombre completo: </b> " + usuario.NombreCompleto;
+                html += "<br><b>Su contraseña temporal es: </b>" + usuario.Password;
+                html += "<br><b>No responda este correo porque fue generado de forma automatica.";
+                html += "<br>Plataforma web biblioteca CR</b>";
+
+                //Se indica que el contenido es en html
+                email.IsBodyHtml = true;
+
+                //Se indica la prioridad del correo, se recomienda usar normal
+                email.Priority = MailPriority.Normal;
+
+                //Se instancia la vista del html para el cuerpo del correo
+                AlternateView view = AlternateView.CreateAlternateViewFromString(html, Encoding.UTF8, MediaTypeNames.Text.Html);
+
+                //Se agrega la view al email
+                email.AlternateViews.Add(view);
+
+                //Se configura el protocolo de comunicacion smtp
+                SmtpClient smtp = new SmtpClient();
+
+                //Se indica el nombre del servidor smtp a sincronizar la cuenta
+                smtp.Host = "smtp-mail.outlook.com";
+
+                //Se indica el puerto de comunicacion
+                smtp.Port = 587;
+
+                //Se indica que se usara el protocolo de seguridad tipo SSL
+                smtp.EnableSsl = true;
+
+                //Se indican las credenciales de la cuenta de correo por default
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential("Lenguajes2023G2@Outlook.com", "Ucr2023*");
+
+                //Se envia el correo
+                smtp.Send(email);
+
+                //Se liberan los procesos
+                email.Dispose();
+                smtp.Dispose();
+
+                
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+    }//Cierre class
+
+}//Cierre namespace
