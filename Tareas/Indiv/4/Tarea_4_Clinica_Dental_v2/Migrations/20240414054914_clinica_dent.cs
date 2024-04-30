53c656c3f603c2d961720b278d5deb901228fbc4
wherrer12 <william.herrera@ucr.ac.cr>
2024-04-30 08:02:56 -0600

    Semana 7 - copia seguridad

---
+++ Tareas/Indiv/4/Tarea_4_Clinica_Dental_v2/Migrations/20240414054914_clinica_dent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tarea_3_Clinica_Dental.Migrations
+{
+    /// <inheritdoc />
+    public partial class clinica_dent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pacientes",
+                columns: table => new
+                {
+                    idPaciente = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cedula = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Procedimiento = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pacientes", x => x.idPaciente);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pacientes");
+        }
+    }
+}
---
+++ Tareas/Indiv/4/Tarea_4_Clinica_Dental_v2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tarea_3_Clinica_Dental.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
